442721b6bd201788ff72e9754f9a70a76f45d6bf
pSpitzner <github@makeitso.one>
2026-07-02 16:28:24 +0200

    use correct build folder, amend update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   url: 'https://coasti.readthedocs.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/en/latest/',
 
   // Required for compatibility with Read the Docs
   trailingSlash: true,
