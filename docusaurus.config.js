c306360eb76f61fe18062128d6d7d99f2a11157b
Jakob Buergermeister <32355048+jbuergermeister@users.noreply.github.com>
2026-07-02 14:30:53 +0200

    Switch to stable packages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,6 +17,7 @@ const config = {
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    faster: false,
   },
 
   // Set the production url of your site here
